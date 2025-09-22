5675b76391db5b0d259c5ace6fdadbe09e2b9380
robertgruevski <robertgruevski@outlook.com>
2025-09-22 16:03:58 -0400

    End of section 03

--- API/Program.cs
+++ API/Program.cs
@@ -7,7 +7,14 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseCors(options =>
+    options.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
