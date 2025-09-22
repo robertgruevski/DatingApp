489de668abafb61c47d499d3b82b9f0c99767584
robertgruevski <robertgruevski@outlook.com>
2025-09-22 15:03:10 -0400

    End of Section 02

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,30 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] // localhost:5001/api/members
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.Users.ToListAsync();
+
+            return members;
+        }
+
+
+        [HttpGet("{id}")]  // localhost:5001/api/members/bob-id
+        public async Task<ActionResult<AppUser>> GetMember(string id)
+        {
+            var member = await context.Users.FindAsync(id);
+
+            return member is null ? NotFound() : member;
+        }
+
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,11 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
