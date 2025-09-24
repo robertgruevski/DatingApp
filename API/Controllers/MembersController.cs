33017a498f5b1a31248f5b4794556b1a67faf041
robertgruevski <robertgruevski@outlook.com>
2025-09-24 17:37:12 -0400

    End of section 10

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,4 +1,7 @@
+using System.Security.Claims;
+using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -28,4 +31,24 @@ public class MembersController(IMemberRepository memberRepository) : BaseApiCont
         return Ok(await memberRepository.GetPhotosForMemberAsync(id));
     }
 
+    [HttpPut]
+    public async Task<ActionResult> UpdateMember(MemberUpdateDto memberUpdateDto)
+    {
+        var memberId = User.GetMemberId();
+
+        var member = await memberRepository.GetMemberForUpdate(memberId);
+
+        if (member is null) return BadRequest("Could not get member");
+
+        member.DisplayName = memberUpdateDto.DisplayName ?? member.DisplayName;
+        member.Description = memberUpdateDto.Description ?? member.Description;
+        member.City = memberUpdateDto.City ?? member.City;
+        member.Country = memberUpdateDto.Country ?? member.Country;
+
+        member.User.DisplayName = memberUpdateDto.DisplayName ?? member.User.DisplayName;
+
+        if (await memberRepository.SaveAllAsync()) return NoContent();
+
+        return BadRequest("Failed to update member");
+    }
 }
