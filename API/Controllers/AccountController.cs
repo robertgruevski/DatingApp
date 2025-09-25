3ba2920093d6f9455eff657662509dccd529816b
robertgruevski <robertgruevski@outlook.com>
2025-09-25 16:14:10 -0400

    End of section 12

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -24,10 +24,19 @@ public class AccountController(AppDbContext context, ITokenService tokenService)
             DisplayName = registerDto.DisplayName,
             Email = registerDto.Email,
             PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-            PasswordSalt = hmac.Key
+            PasswordSalt = hmac.Key,
+            Member = new Member
+            {
+                DisplayName = registerDto.DisplayName,
+                Gender = registerDto.Gender,
+                City = registerDto.City,
+                Country = registerDto.Country,
+                DateOfBirth = registerDto.DateOfBirth
+            }
         };
 
         context.Users.Add(user);
+
         await context.SaveChangesAsync();
 
         return user.ToDto(tokenService);
--- client/src/features/members/member-photos/member-photos.ts
+++ client/src/features/members/member-photos/member-photos.ts
@@ -39,6 +39,9 @@ export class MemberPhotos implements OnInit {
         this.memberService.editMode.set(false);
         this.loading.set(false);
         this.photos.update((photos) => [...photos, photo]);
+        if (!this.memberService.member()?.imageUrl) {
+          this.setMainLocalPhoto(photo);
+        }
       },
       error: (error) => {
         console.log('Error uploading image: ', error);
@@ -50,19 +53,7 @@ export class MemberPhotos implements OnInit {
   setMainPhoto(photo: Photo) {
     this.memberService.setMainPhoto(photo).subscribe({
       next: () => {
-        const currentUser = this.accountService.currentUser();
-
-        if (currentUser) currentUser.imageUrl = photo.url;
-
-        this.accountService.setCurrentUser(currentUser as User);
-
-        this.memberService.member.update(
-          (member) =>
-            ({
-              ...member,
-              imageUrl: photo.url,
-            } as Member)
-        );
+        this.setMainLocalPhoto(photo);
       },
     });
   }
@@ -73,5 +64,21 @@ export class MemberPhotos implements OnInit {
         this.photos.update((photos) => photos.filter((x) => x.id !== photoId));
       },
     });
+  }
+
+  private setMainLocalPhoto(photo: Photo) {
+    const currentUser = this.accountService.currentUser();
+
+    if (currentUser) currentUser.imageUrl = photo.url;
+
+    this.accountService.setCurrentUser(currentUser as User);
+
+    this.memberService.member.update(
+      (member) =>
+        ({
+          ...member,
+          imageUrl: photo.url,
+        } as Member)
+    );
   }
 }
