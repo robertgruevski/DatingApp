d049aabf178c675d367c4bf64fd3a6c064fab4dd
robertgruevski <robertgruevski@outlook.com>
2025-09-26 15:29:49 -0400

    End of section 14

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -3,16 +3,16 @@ import { inject, Injectable, signal } from '@angular/core';
 import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 import { environment } from '../../environments/environment.development';
+import { LikesService } from './likes-service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AccountService {
   baseUrl = environment.apiUrl;
-
   currentUser = signal<User | null>(null);
-
   private http = inject(HttpClient);
+  private likesService = inject(LikesService);
 
   register(creds: RegisterCreds) {
     return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
@@ -37,11 +37,13 @@ export class AccountService {
   setCurrentUser(user: User) {
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUser.set(user);
+    this.likesService.getLikeIds();
   }
 
   logout() {
     localStorage.removeItem('user');
     localStorage.removeItem('filters');
+    this.likesService.clearLikeIds();
     this.currentUser.set(null);
   }
 }
